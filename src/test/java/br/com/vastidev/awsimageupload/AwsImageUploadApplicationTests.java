1046d28498ad93efb693aa7a4ca54fbcb557ed0b
VastiDev <vastiane1@gmail.com>
2023-09-07 21:13:08 -0300

    modifica pom e inicia o projeto

---
+++ src/test/java/br/com/vastidev/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vastidev.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
